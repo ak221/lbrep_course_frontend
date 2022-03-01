58a8a4bea3278c48ca6cfbee27b155037ac81909
Karim <karimdiouf221@gmail.com>
2022-03-01 18:36:46 +0000

    changed zzom level

--- src/Components/Listings.js
+++ src/Components/Listings.js
@@ -221,7 +221,7 @@ function Listings() {
 					<div style={{ height: "100vh" }}>
 						<MapContainer
 							center={[51.505, -0.09]}
-							zoom={14}
+							zoom={12}
 							scrollWheelZoom={true}
 						>
 							<TileLayer
